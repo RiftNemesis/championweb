21612fe1196286f7dfd5430860cabec20fd35d7c
Dan Pantry <dan.pantry@hotmail.com>
2015-06-20 16:43:38 +0100

    chore(gruntfile): fix whitespacing

--- gruntfile.js
+++ gruntfile.js
@@ -19,14 +19,14 @@ module.exports = function(grunt){
 		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			files: [
-					'routes/*.js',
-					'bin/*.js',
-					'logic/*.js',
-					'models/*.js',
-					'public/js/*.js',
-					'!public/js/master.min.js',
-					'*.js' 
-				],
+				'routes/*.js',
+				'bin/*.js',
+				'logic/*.js',
+				'models/*.js',
+				'public/js/*.js',
+				'!public/js/master.min.js',
+				'*.js' 
+			],
 			options: {
 			// options here to override JSHint defaults
 				node: true,
@@ -40,28 +40,37 @@ module.exports = function(grunt){
 			}
 		},
 		concat: {
-		    js: {
-		      	src: ['public/dist/js/angular.js', 'public/dist/js/angular-bootstrap.js', 
-		      		'public/dist/js/dirDisqus.js', 'public/dist/js/chart.js',
-		      		'public/dist/js/tc-angular-chartjs.js', 'public/js/champion_data.js', 'public/js/chart_options.js', 'public/js/championgg_tooltip.js',
-		      		 'public/js/app.js', 'public/js/champion_page.js', 'public/js/matchup_page.js', 'public/js/statistics_page.js'],
-		      	dest: 'public/js/master.min.js'
-		    },
+	    js: {
+      	src: [
+      		'public/dist/js/angular.js', 
+      		'public/dist/js/angular-bootstrap.js', 
+      		'public/dist/js/dirDisqus.js', 
+      		'public/dist/js/chart.js',
+      		'public/dist/js/tc-angular-chartjs.js', 
+      		'public/js/champion_data.js', 
+      		'public/js/chart_options.js', 
+      		'public/js/championgg_tooltip.js',
+      		'public/js/app.js', 
+      		'public/js/champion_page.js', 
+      		'public/js/matchup_page.js', 
+      		'public/js/statistics_page.js'],
+      	dest: 'public/js/master.min.js'
+	    },
 		},
-	    uglify: {
+    uglify: {
 			build:  {
-			    files: {
-			      	'public/js/master.min.js': 'public/js/master.min.js'
-			    }
+		    files: {
+	      	'public/js/master.min.js': 'public/js/master.min.js'
+		    }
 			}
 		},
 		cssmin: {
-		    build: {
-		        src: ['public/css/master.css', 'public/css/sprite.css'],
-		        dest: 'public/css/master.min.css'
-		    }
-    	}
-    });
+	    build: {
+        src: ['public/css/master.css', 'public/css/sprite.css'],
+        dest: 'public/css/master.min.css'
+	    }
+  	}
+  });
 	
 	grunt.registerTask('production', ['concat', 'uglify','cssmin']);
 	grunt.registerTask('default', []);
